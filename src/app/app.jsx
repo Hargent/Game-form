dcca481aae59cefab9dc947084080661da8413d9
Hargent <yemisi3385@gmail.com>
2023-08-17 18:53:54 +0100

    feat(Project setup): Project starter files created as well as routes setup
    chore(tools): use react-router, formik. context api, context hooks and reducer hook

---
+++ src/app/app.jsx
@@ -0,0 +1,15 @@
+import Navigation from "../route/navigation";
+import React from "react";
+
+const app = () => {
+	return (
+		<div className="app">
+			SideBar
+			<Navigation />
+		</div>
+	);
+};
+/*
+use router,use reducer & useContext
+*/
+export default app;
